b07ae0115acfb271726d9c24c3a63c6d3bac22f5
Stefan Rotsch <stefan.rotsch@aoe.com>
2018-01-10 17:03:30 +0100

    [TASK] Custom command should be available without setting up TYPO3

--- Configuration/Console/Commands.php
+++ Configuration/Console/Commands.php
@@ -4,6 +4,7 @@ return [
         Srotsch\HelloT3console\Command\HelloCommandController::class
     ],
     'runLevels' => [
+        'hello_t3console:hello:helloworld' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE
     ],
     'bootingSteps' => [
     ]
